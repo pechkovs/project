6b9efd4ae4b0cdc1b8fd926249f9de630f3c2c17
Sergei Pechkov <segapechkov@mail.ru>
2022-10-17 12:36:10 +0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './app';
+
+export default () => <App/>;
+
+export const mount = (Сomponent) => {
+    ReactDom.render(
+        <Сomponent/>,
+        document.getElementById('app')
+    );
+
+    if(module.hot) {
+        module.hot.accept('./app', ()=> {
+            ReactDom.render(
+                <App/>,
+                document.getElementById('app')
+            );
+        })
+    }
+};
+
+export const unmount = () => {
+    ReactDom.unmountComponentAtNode(document.getElementById('app'));
+};
+
