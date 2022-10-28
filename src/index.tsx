ba6e7a6b2c24e909fa264129719b5d07acbff256
svchesnovski <svchesnovski@gmail.com>
2022-10-28 21:01:46 +0300

    Подключен prettier и eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,22 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from 'react'
+import ReactDom from 'react-dom'
 
-import App from './app';
+import App from './app'
 
-export default () => <App/>;
+// это не компонент, а фyнкция
+// eslint-disable-next-line react/display-name
+export default () => <App />
 
 export const mount = (Сomponent) => {
-    ReactDom.render(
-        <Сomponent/>,
-        document.getElementById('app')
-    );
+    ReactDom.render(<Сomponent />, document.getElementById('app'))
 
-    if(module.hot) {
-        module.hot.accept('./app', ()=> {
-            ReactDom.render(
-                <App/>,
-                document.getElementById('app')
-            );
+    if (module.hot) {
+        module.hot.accept('./app', () => {
+            ReactDom.render(<App />, document.getElementById('app'))
         })
     }
-};
+}
 
 export const unmount = () => {
-    ReactDom.unmountComponentAtNode(document.getElementById('app'));
-};
-
+    ReactDom.unmountComponentAtNode(document.getElementById('app'))
+}
